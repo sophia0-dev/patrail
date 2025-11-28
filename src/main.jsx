5d4e20bdcefa05898cda2d0be8746b9509573cba
czDamian <olebueziedamian@gmail.com>
2025-11-28 15:55:27 +0100

    added tailwindcss correctly, updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,22 @@
-import './index.css'
-import React from 'react'
-import App from './App'
-import reactDom from 'react-dom';
+// import './index.css'
+// import React from 'react'
+// import App from './App'
+// import reactDom from 'react-dom';
 
-reactDom.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
-)
+// reactDom.render(
+//     <React.StrictMode>
+//         <App/>
+//     </React.StrictMode>,
+//     document.getElementById('root')
+// )
 
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
